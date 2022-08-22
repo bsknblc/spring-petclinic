258a824f6f142e11bc33d9f1b5dd8de60e715bee
Başak Balcı <60985930+bsknblc@users.noreply.github.com>
2022-08-22 19:46:29 +0300

    initial stage

--- src/test/java/org/springframework/samples/petclinic/test/ImpactAnalyzerTest.java
+++ src/test/java/org/springframework/samples/petclinic/test/ImpactAnalyzerTest.java
@@ -17,6 +17,8 @@ import org.springframework.samples.petclinic.ImpactAnalyzer.models.TestElement;
 import org.springframework.samples.petclinic.ImpactAnalyzer.models.TestFileParser;
 import org.springframework.samples.petclinic.test.DependencyAnalyzer;
 
+
+
 public class ImpactAnalyzerTest {
 
 	@org.junit.Test
