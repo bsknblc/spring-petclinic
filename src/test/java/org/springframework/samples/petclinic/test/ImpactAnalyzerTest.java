73c7209945ad354cef21e9897b3af1b28355f7c6
Başak Balcı <60985930+bsknblc@users.noreply.github.com>
2022-08-22 22:59:41 +0300

    initial

--- src/test/java/org/springframework/samples/petclinic/test/ImpactAnalyzerTest.java
+++ src/test/java/org/springframework/samples/petclinic/test/ImpactAnalyzerTest.java
@@ -17,6 +17,8 @@ import org.springframework.samples.petclinic.ImpactAnalyzer.models.TestElement;
 import org.springframework.samples.petclinic.ImpactAnalyzer.models.TestFileParser;
 import org.springframework.samples.petclinic.test.DependencyAnalyzer;
 
+// Initial State of the program
+
 public class ImpactAnalyzerTest {
 
 	@org.junit.Test
